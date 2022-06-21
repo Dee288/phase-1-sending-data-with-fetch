8fc64b558446232b73c9ea1f640c06b8249cffbb
dee288 <dianamajala10@gmail.com>
2022-06-21 14:34:07 +0300

    intial commit

--- index.js
+++ index.js
@@ -5,18 +5,59 @@ return fetch(" http://localhost:3000/users", {
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-  },
+},
   body: JSON.stringify({
-    name,
-    email,
+    name: 'diana',
+    email: 'diana@diana.com'
 })
-.then(function (response) {
+}).then(function (response) {
     return response.json();
   })
   .then(function (users) {
     document.body.innerHTML=users['id']
   })
   .catch(function (error) { document.body.innerHTML = error.message})
-})
 }
-    
+
+
+
+/* the other solution
+function submitData (name, email){
+
+ fetch("function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      "method": "POST",
+      "body": {
+        name ,
+        email
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(users) {
+      document.body.innerHTML=users['id'];
+      }).catch(function(error ) {document.body.innerHTML = error.message})
+      
+      return ":(";
+    };
+    , configurationObject);
+ const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name,
+    email,
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (users) {
+    document.body.innerHTML=users['id']
+  })
+  .catch(function (error) { document.body.innerHTML = error.message})
+}
+*/
+
+  
